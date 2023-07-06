d88f7812f1e367ad508f2fb96a396c13f5ae4b88
Julien Dominguez <dominguez.julien@gmail.com>
2023-07-06 23:18:52 +0200

    commit fetch api musee - composant artwork avec image

--- components/Artwork.js
+++ components/Artwork.js
@@ -1,21 +1,30 @@
-import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { TouchableOpacity, StyleSheet, Text, View, Image } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-
-export default function Artwork(props) {
-    return (
-    <TouchableOpacity style={styles.container}>
-
-    </TouchableOpacity>
+export default function Artwork({image}) {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{
+            uri: image,
+          }}      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'green',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: `80%`,
-      },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 400,
+    height: 400,
+  },
+
+  image: {
+    width: 150,
+    height: 150,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,67 @@
-import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View, SafeAreaView, scrollView, ScrollView } from "react-native";
+import Artwork from "../components/Artwork";
 // api = https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&limit=200&female_artists=none
-
 
 export default function HomeScreen({ navigation }) {
   const [artworksData, setArtworksData] = useState([]);
 
   useEffect(() => {
-    fetch('https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&limit=200&female_artists=none')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        // const formatedData = data.movies.map(movie => {
-        //   let overview = movie.overview;
-        //   return { title: movie.title, poster, voteAverage: movie.vote_average, voteCount: movie.vote_count, overview };
-        // });
-        // setArtworksData(formatedData);
+    fetch(
+      "https://openaccess-api.clevelandart.org/api/artworks/?has_image=1&limit=10&female_artists=none"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("author is", data.data[0].creators[0].description);
+        const formatedData = data.data.map((artwork) => {
+          let author = artwork.creators[0].description;
+          let date = artwork.creation_date;
+          let title = artwork.title;
+          let image = artwork.images.web.url;
+          let technique = artwork.technique;
+          let type = artwork.type;
+          let description = artwork.wall_description;
+          let funFact = artwork.fun_fact;
+          let department = artwork.department;
+          return {
+            author,
+            date,
+            title,
+            image,
+            technique,
+            type,
+            description,
+            funFact,
+            department,
+          };
+        });
+        console.log("formated data is", formatedData);
+        setArtworksData(formatedData);
       });
   }, []);
 
- return (
-   <SafeAreaView style={styles.container}>
-     <Text>Home Screen</Text>
-   </SafeAreaView>
- );
+  const artworks = artworksData.map((data, i) => {
+    console.log('map data is', data)
+    return <Artwork key={i} image={data.image} />;
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Home Screen</Text>
+      <ScrollView>{artworks}</ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'yellow',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-   });
+  container: {
+    flex: 1,
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  scrollView: {
+    alignItems: 'center',
+  },
+});
